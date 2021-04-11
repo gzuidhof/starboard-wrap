74d0887a44dd8b34c81a655204d4cc5290013048
Guido Zuidhof <me@guido.io>
2021-04-11 17:29:36 +0100

    Include Polyfill for Safari

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
+// Polyfill required for Safari
+import "@ungap/custom-elements";
 export {StarboardNotebookIFrame} from "./embed";
