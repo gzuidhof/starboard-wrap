89e7ff1fd5bcf88da94fe5d79e2f46999fa28582
Guido Zuidhof <me@guido.io>
2021-05-11 21:19:24 +0100

    Release 0.3.0

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # 🌯 starboard-wrap
-A small library that wraps a Starboard Notebook iframe in the parent webpage
+A small library that wraps a Starboard Notebook iframe in the parent webpage.
 
 ## What?
 
@@ -13,16 +13,39 @@ The webpage talks to the notebook runtime in the iFrame using `postMessage`. Thi
 <div id="mount-point"></div>
 
 <script type="module">
-    import {StarboardNotebookIFrame} from "../dist/index.js"
+    import {StarboardEmbed} from "../dist/index.js"
     const mount = document.querySelector("#mount-point");
 
-    const el = new StarboardNotebookIFrame({
+    const el = new StarboardEmbed({
         notebookContent: "# %% [javascript]\n3+5\n",
-        src: "https://unpkg.com/starboard-notebook@0.7.12/dist/index.html"
+        src: "https://unpkg.com/starboard-notebook@0.9.1/dist/index.html"
     });
 
     el.style.width = "100%";
     mount.appendChild(el);
 </script>
-
 ```
+
+Or
+
+```html
+<script src="/dist/index.iife.js" defer></script>
+
+<starboard-embed>
+
+    <script type="starboard">
+# %% [javascript]
+3+5
+</script>
+
+<iframe src="https://unpkg.com/starboard-notebook@0.9.1/dist/index.html" style="width: 100%"></iframe>
+</starboard-embed>
+```
+
+## Changelog
+
+### 0.3.0
+* Renamed the custom element to `StarboardEmbed` with HTML tag `starboard-embed`.
+* The custom element longer extends `IFrameHTMLElement`, instead it will look for an iframe child node or create one if it doesn't exist. This means that the Safari polyfill is no longer required, and pages containing a notebook in an iframe will load faster as the iframe can start loading before the main page's Javascript has been run.
+* You can now embed the notebook's content in a script tag inside the `starboard-embed` element.
+* `sendMessage()` function now buffers messages until the notebook ready signal has been received.
--- src/embed.ts
+++ src/embed.ts
@@ -3,118 +3,169 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
 // @ts-ignore - no types available for iframe-resizer
-import {default as iFrameResizer} from "iframe-resizer/js/iframeResizer";
+import { default as iFrameResizer } from "iframe-resizer/js/iframeResizer";
 
-import {ContentUpdateMessage, InboundNotebookMessage, NotebookMessage, OutboundNotebookMessage, ReadySignalMessage, SaveMessage} from "starboard-notebook/dist/src/messages/types"
+import {
+  ContentUpdateMessage,
+  InboundNotebookMessage,
+  NotebookMessage,
+  OutboundNotebookMessage,
+  ReadySignalMessage,
+  SaveMessage,
+} from "starboard-notebook/dist/src/messages/types";
+import { flatPromise } from "./flatPromise";
 
 export type StarboardNotebookIFrameOptions<ReceivedMessageType = OutboundNotebookMessage> = {
-    src: string;
+  src: string;
 
-    autoResize: boolean;
-    inPageLinks: boolean;
+  autoResize: boolean;
+  inPageLinks: boolean;
 
-    baseUrl?: string;
+  baseUrl?: string;
+  notebookContent?: Promise<string> | string;
 
-    notebookContent?: Promise<string> | string;
+  onNotebookReadySignalMessage(payload: ReadySignalMessage["payload"]): void;
+  onSaveMessage(payload: SaveMessage["payload"]): void;
+  onContentUpdateMessage(payload: ContentUpdateMessage["payload"]): void;
+  onMessage(message: ReceivedMessageType): void;
 
-    onNotebookReadySignalMessage(payload: ReadySignalMessage['payload']): void;
-    onSaveMessage(payload: SaveMessage['payload']): void;
-    onContentUpdateMessage(payload: ContentUpdateMessage['payload']): void;
-    onMessage(message: ReceivedMessageType): void;
-
-    sandbox: string;
-    debug: boolean;
-}
+  sandbox: string;
+  debug: boolean;
+};
 
 export type StarboardNotebookMessage = {
-    type: "SIGNAL_READY" | "SET_NOTEBOOK_CONTENT" | "NOTEBOOK_CONTENT_UPDATE" | "SAVE";
+  type: "SIGNAL_READY" | "SET_NOTEBOOK_CONTENT" | "NOTEBOOK_CONTENT_UPDATE" | "SAVE";
+};
+
+function loadDefaultSettings(
+  opts: Partial<StarboardNotebookIFrameOptions>,
+  el: HTMLIFrameElement
+): StarboardNotebookIFrameOptions {
+  return {
+    src:
+      opts.src ??
+      el.getAttribute("src") ??
+      (window as any).starboardIFrameSrc ??
+      "starboard-notebook-iframe-src-not-set",
+    baseUrl: opts.baseUrl || el.dataset["baseUrl"] || undefined,
+    autoResize: opts.autoResize ?? true,
+    inPageLinks: opts.inPageLinks ?? true,
+    sandbox:
+      opts.sandbox ??
+      el.getAttribute("sandbox") ??
+      "allow-scripts allow-modals allow-same-origin allow-pointer-lock allow-top-navigation-by-user-activation allow-forms allow-downloads",
+    debug: opts.debug ?? false,
+    onNotebookReadySignalMessage: opts.onNotebookReadySignalMessage ?? function () {},
+    onContentUpdateMessage: opts.onContentUpdateMessage ?? function () {},
+    onSaveMessage: opts.onSaveMessage ?? function () {},
+    onMessage: opts.onMessage ?? function () {},
+    notebookContent: opts.notebookContent,
+  };
 }
 
-function loadDefaultSettings(opts: Partial<StarboardNotebookIFrameOptions>, el: StarboardNotebookIFrame): StarboardNotebookIFrameOptions {
-    return {
-        // We fall back to a specific starboard package version on jsdelivr, but ideally one would always specify their own URL that is locked to a specific version.
-        src: opts.src ?? el.getAttribute("src") ?? "starboard-notebook-iframe-src-not-set",
-        baseUrl: opts.baseUrl || el.dataset["baseUrl"] || undefined,
-        autoResize: opts.autoResize ?? true,
-        inPageLinks: opts.inPageLinks ?? true,
-        sandbox: opts.sandbox ?? el.getAttribute("sandbox") ?? "allow-scripts allow-modals allow-same-origin allow-pointer-lock allow-top-navigation-by-user-activation allow-forms allow-downloads",
-        debug: opts.debug ?? false,
-        onNotebookReadySignalMessage: opts.onNotebookReadySignalMessage ?? function(){},
-        onContentUpdateMessage: opts.onContentUpdateMessage ?? function(){},
-        onSaveMessage: opts.onSaveMessage ?? function(){},
-        onMessage: opts.onMessage ?? function(){},
-        notebookContent: opts.notebookContent
-    }
-} 
+export class StarboardEmbed extends HTMLElement {
+  private options?: StarboardNotebookIFrameOptions;
+  private constructorOptions: Partial<StarboardNotebookIFrameOptions>;
+  public notebookContent: string = "";
 
-export class StarboardNotebookIFrame extends HTMLIFrameElement {
-    private options?: StarboardNotebookIFrameOptions;
-    private constructorOptions: Partial<StarboardNotebookIFrameOptions>;
-    public notebookContent: string = "";
+  // The version of starboard-wrap
+  public version: string = "__STARBOARD_WRAP_VERSION__";
 
-    // The version of starboard-wrap
-    public version: string = "__STARBOARD_WRAP_VERSION__";
+  /**
+   * This is set automatically by iframeResizer.
+   */
+  private iFrameResizer!: any; // TODO check if this is always a list,
 
-    /**
-     * This is set automatically by iframeResizer.
-     */
-    private iFrameResizer!: any; // TODO check if this is always a list, 
+  /**
+   * The wrapped iframe element.
+   */
+  private iFrame: HTMLIFrameElement | null;
 
-    constructor(opts: Partial<StarboardNotebookIFrameOptions> = {}) {
-        super();
-        this.constructorOptions = opts;
-    }
+  private hasReceivedReadyMessage = flatPromise();
 
-    connectedCallback() {
-        this.options = loadDefaultSettings(this.constructorOptions, this);
-        const checkOrigin = [new URL(this.options.src).origin];
+  constructor(opts: Partial<StarboardNotebookIFrameOptions> = {}) {
+    super();
+    this.constructorOptions = opts;
 
-        this.sandbox.value = this.options.sandbox;
-        this.src = this.options.src;
-        this.frameBorder = "0";
+    this.iFrame = this.querySelector("iframe");
+  }
 
-        iFrameResizer({
-            autoResize: this.options.autoResize, 
-            inPageLinks: this.options.inPageLinks,
-            checkOrigin: checkOrigin,
-            log: this.options.debug,
-            onMessage: async (data: {iframe: any, message: OutboundNotebookMessage}) => {
-                const msg = data.message;
-                if (msg.type === "NOTEBOOK_READY_SIGNAL") {
-                    if (this.options!.notebookContent) {
-                        const content = await this.options!.notebookContent;
-                        this.sendMessage({
-                            type: "NOTEBOOK_SET_INIT_DATA", payload: {content, baseUrl: this.options!.baseUrl}
-                        });
-                    } else {
-                        this.notebookContent = msg.payload.content;
-                    }
-                    this.options!.onNotebookReadySignalMessage(msg.payload);
-                } else if (msg.type === "NOTEBOOK_CONTENT_UPDATE") {
-                    this.notebookContent = msg.payload.content;
-                    this.options!.onContentUpdateMessage(msg.payload);
-                } else if (msg.type === "NOTEBOOK_SAVE_REQUEST") {
-                    this.notebookContent = msg.payload.content;
-                    this.options!.onSaveMessage(msg.payload);
-                }
-                this.options!.onMessage(msg);
-            },
-            onReady: () => {},
-        }, this)
+  connectedCallback() {
+    // Find iframe element child, and otherwise create one.
+    this.iFrame = this.querySelector("iframe");
+    if (!this.iFrame) {
+      this.iFrame = document.createElement("iframe");
+      this.iFrame.style.width = "100%";
+      this.appendChild(this.iFrame);
     }
 
-    public sendMessage(message: InboundNotebookMessage) {
-        this.iFrameResizer.sendMessage(message);
+    this.options = loadDefaultSettings(this.constructorOptions, this.iFrame);
+    if (!this.options.notebookContent) {
+      const scriptEl = this.querySelector("script");
+      if (scriptEl) {
+        this.options.notebookContent = scriptEl.innerText;
+      }
     }
 
-    public sendCustomMessage(message: NotebookMessage<string, any>) {
-        this.sendMessage(message as any);
-    }
+    const checkOrigin = [new URL(this.options.src).origin];
 
-    public dispose() {
-        this.iFrameResizer.close();
-    }
+    this.iFrame.sandbox.value = this.options.sandbox;
 
+    // Without this check it will reload the page
+    if (this.iFrame.src !== this.options.src) {
+      this.iFrame.src = this.options.src;
+    }
+    this.iFrame.frameBorder = "0";
+
+    const ifr = iFrameResizer(
+      {
+        autoResize: this.options.autoResize,
+        inPageLinks: this.options.inPageLinks,
+        checkOrigin: checkOrigin,
+        log: this.options.debug,
+        onMessage: async (data: { iframe: any; message: OutboundNotebookMessage }) => {
+          const msg = data.message;
+          if (msg.type === "NOTEBOOK_READY_SIGNAL") {
+            if (this.options!.notebookContent) {
+              const content = await this.options!.notebookContent;
+              this.sendMessage({
+                type: "NOTEBOOK_SET_INIT_DATA",
+                payload: { content, baseUrl: this.options!.baseUrl },
+              });
+            } else {
+              this.notebookContent = msg.payload.content;
+            }
+            this.hasReceivedReadyMessage.resolve(msg.payload);
+            this.options!.onNotebookReadySignalMessage(msg.payload);
+          } else if (msg.type === "NOTEBOOK_CONTENT_UPDATE") {
+            this.notebookContent = msg.payload.content;
+            this.options!.onContentUpdateMessage(msg.payload);
+          } else if (msg.type === "NOTEBOOK_SAVE_REQUEST") {
+            this.notebookContent = msg.payload.content;
+            this.options!.onSaveMessage(msg.payload);
+          }
+          this.options!.onMessage(msg);
+        },
+        onReady: () => {},
+      },
+      this.iFrame
+    );
+
+    this.iFrameResizer = ifr[0].iFrameResizer;
+  }
+
+  public sendMessage(message: InboundNotebookMessage) {
+    // Sending messages before the iframe leads to messages being lost, which can happen when the iframe loads slowly.
+    this.hasReceivedReadyMessage.promise.then(() => this.iFrameResizer.sendMessage(message));
+  }
+
+  public sendCustomMessage(message: NotebookMessage<string, any>) {
+    this.sendMessage(message as any);
+  }
+
+  public dispose() {
+    this.iFrameResizer.close();
+  }
 }
 
-customElements.define('starboard-notebook-iframe', StarboardNotebookIFrame, {extends: 'iframe'})
+customElements.define("starboard-embed", StarboardEmbed);
